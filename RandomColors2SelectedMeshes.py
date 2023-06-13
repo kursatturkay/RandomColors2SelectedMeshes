fec8bfc77f4002da6d795061e754c15cb38da9bb
Kursat Turkay <32601412+kursatturkay@users.noreply.github.com>
2023-06-14 00:29:49 +0300

    Update RandomColors2SelectedMeshes.py

--- RandomColors2SelectedMeshes.py
+++ RandomColors2SelectedMeshes.py
@@ -1,10 +1,8 @@
 import bpy
 import random
 
-# Seçili objeleri alın
 selected_objects = bpy.context.selected_objects
 
-# Her bir seçili objeye unique bir materyal ata ve rastgele renk ver
 for i, obj in enumerate(selected_objects):
     if obj.type == 'MESH':
 
@@ -14,10 +12,8 @@ for i, obj in enumerate(selected_objects):
         material = bpy.data.materials.new(name=f"RandomColor_{i}")
         
         obj.data.materials.append(material)
-        # Rastgele renk oluştur
         random_color = (random.random(), random.random(), random.random(), 1.0)
 
-        # Materyale rastgele renk ata
         material.use_nodes = True
         nodes = material.node_tree.nodes
         principled_bsdf = nodes.get("Principled BSDF")
@@ -26,5 +22,4 @@ for i, obj in enumerate(selected_objects):
         if principled_bsdf and output:
             principled_bsdf.inputs['Base Color'].default_value = random_color
 
-# Ekrana güncellemeleri yansıt
 bpy.context.view_layer.update()
